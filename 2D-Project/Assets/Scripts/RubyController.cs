14201ab8161089ccde1ab3a30d8beb33dc23eedd
krios17 <krios@colum.edu>
2021-10-11 16:33:39 -0500

    World Design
    did the thing

--- 2D-Project/Assets/Scripts/RubyController.cs
+++ 2D-Project/Assets/Scripts/RubyController.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    // Movement
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Debug.Log("horizontal" + horizontal);
+        Debug.Log("vertical" + vertical);
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
