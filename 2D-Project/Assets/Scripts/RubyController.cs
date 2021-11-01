25ec5d71d84fa6296bfaa42c263d57ed5208e535
krios17 <krios@colum.edu>
2021-11-01 14:30:31 -0500

    Cinemachine and HUD

--- 2D-Project/Assets/Scripts/RubyController.cs
+++ 2D-Project/Assets/Scripts/RubyController.cs
@@ -86,6 +86,8 @@ public class RubyController : MonoBehaviour
         }
 
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 
